02f17e3aa3c0d981a161b7805c3eae18246cd59c
barck <barck@MacBook-Pro-barck.local>
2018-02-01 17:37:19 +0500

    next stage

--- index.js
+++ index.js
@@ -3,43 +3,49 @@ import _ from 'underscore';
 import Backbone from 'backbone';
 import template from "./book.html";
 
-
-
-const LinkModel = Backbone.Model.extend({ });
-
-// const LinkView = Backbone.View.extend({
-//   tagName: "li",
-//   className: "list-item",
-//   template: _.template($('#bookTemplate').html()),
-//     render: function () {
-//         this.$el.html(this.template(this.model.toJSON()));
-//         return this;
-//     }
-// });
-
-var LinkView = Backbone.View.extend({
-    tagName: 'div',
-    className: 'book-item',
-    template: _.template($('#bookTemplate').html()),
-
-    render: function() {
-        this.$el.html(this.template(this.model.toJSON()));
-        return this;
+const LinkModel = Backbone.Model.extend({
+    pathGenerator: function() {
+        this.set("imgPath", (this.get("imgLink") + '.jpg'));
+    },
+    defaults: {
+        imgType: '.jpg'
     }
+});
+
+const LinkView = Backbone.View.extend({
+  initialize: function() {
+      this.model.pathGenerator()
+  },
+  tagName: "li",
+  className: "list-item",
+  render: function() {
+   this.$el.html(template(this.model.attributes));
+   return this;
+  }
 });
 
 const Library = Backbone.Collection.extend({
   model: LinkModel
 });
 
-const Router = Backbone.Router.extended({
-   routes: ""
+const Router = Backbone.Router.extend({
+    initialize: function () {
+
+    },
+   routes: {
+       '': 'index'
+   },
+    index: function () {
+        console.log('index')
+    }
 });
 
+new Router();
+
+Backbone.history.start();
 
 window.library = new Library();
-var view = new LinkView()
-new LinkView();
+window.library.on('add', (model) => {(new LinkView({model:model})).render().$el.appendTo('.list')});
 window.library.fetch({ url: '/data.json' });
 
 
