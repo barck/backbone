1f96e04e3993d082fdff288217da6609ec96f91d
barck <artiv09@gmail.com>
2018-02-05 23:20:06 +0500

    second model added

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import $ from 'jquery';
 import _ from 'underscore';
 import Backbone from 'backbone';
-import template from "./book.html";
+import bookTemplate from "./book.html";
 
 //создаем модель данных
 const LinkModel = Backbone.Model.extend({
+    defaults: {
+
+    }
+});
+
+const OneModel = Backbone.Model.extend({
     defaults: {
 
     }
@@ -18,22 +24,48 @@ const LinkView = Backbone.View.extend({
   className: "list-item",
   events: {
       //событие клик на элементе с классом .link, вызов метода navigate
-    "click .link": "navigate"
+    "click .link": "navigate",
+    "click .remove": "removeModel"
   },
     //метод, выполняющийся при ивенте "click"
   navigate: function () {
     router.navigate("post/" + this.model.get("id"), {trigger: true});
-    alert(this.model.get("title"));
+    //this.remove();  как удалить все вьюхи?
+    let oneLibrary = new OneLibrary();    //создаем экземпляр коллекции
+    oneLibrary.on('add', (model) => {(new OneView({model:model})).render().$el.appendTo('.list')});
+    oneLibrary.fetch({ url: 'https://jsonplaceholder.typicode.com/posts/' + this.model.get("id") });
+    //library.remove(this)
+
+  },
+  removeModel: function () {
+      this.remove()
   },
   render: function() {
-   this.$el.html(template(this.model.attributes));
+   this.$el.html(bookTemplate(this.model.attributes));
    return this;
   }
+});
+
+const OneView = Backbone.View.extend({
+    initialize: function(options) {
+    },
+    tagName: "li",
+    className: "list-item",
+    events: {
+    },
+    render: function() {
+        this.$el.html(bookTemplate(this.model.attributes));
+        return this;
+    }
 });
 
 //создаем коллекцию экземпляров модели
 const Library = Backbone.Collection.extend({
   model: LinkModel
+});
+
+const OneLibrary = Backbone.Collection.extend({
+    model: OneModel
 });
 
 
@@ -46,7 +78,7 @@ let Router = Backbone.Router.extend({
        console.log("index")
     },
     post: function () {
-        alert(a)
+        alert(123);
     }
 });
 
@@ -58,5 +90,7 @@ Backbone.history.start({pushState: true});
 let library = new Library();    //создаем экземпляр коллекции
 library.on('add', (model) => {(new LinkView({model:model})).render().$el.appendTo('.list')});
 library.fetch({ url: 'https://jsonplaceholder.typicode.com/posts' });
+
+
 
 
