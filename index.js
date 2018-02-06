27b484b29dffd8a8c5a702a0f906c2a943fc7ea3
barck <artiv09@gmail.com>
2018-02-06 17:54:07 +0500

    new dir system

--- index.js
+++ index.js
@@ -1,41 +1,21 @@
 import $ from 'jquery';
 import _ from 'underscore';
 import Backbone from 'backbone';
-import bookTemplate from "./book.html";
+import bookTemplate from "./app/templates/book.html";
 
 //создаем модель данных
-const LinkModel = Backbone.Model.extend({
-    defaults: {
-
-    }
-});
-
-const OneModel = Backbone.Model.extend({
-    defaults: {
-
-    }
-});
+const LinkModel = Backbone.Model.extend();
 
 //создаем вьюху
 const LinkView = Backbone.View.extend({
-  initialize: function(options) {
-  },
   tagName: "li",
   className: "list-item",
   events: {
-      //событие клик на элементе с классом .link, вызов метода navigate
     "click .link": "navigate",
     "click .remove": "removeModel"
   },
-    //метод, выполняющийся при ивенте "click"
   navigate: function () {
     router.navigate("post/" + this.model.get("id"), {trigger: true});
-    //this.remove();  как удалить все вьюхи?
-    let oneLibrary = new OneLibrary();    //создаем экземпляр коллекции
-    oneLibrary.on('add', (model) => {(new OneView({model:model})).render().$el.appendTo('.list')});
-    oneLibrary.fetch({ url: 'https://jsonplaceholder.typicode.com/posts/' + this.model.get("id") });
-    //library.remove(this)
-
   },
   removeModel: function () {
       this.remove()
@@ -46,27 +26,11 @@ const LinkView = Backbone.View.extend({
   }
 });
 
-const OneView = Backbone.View.extend({
-    initialize: function(options) {
-    },
-    tagName: "li",
-    className: "list-item",
-    events: {
-    },
-    render: function() {
-        this.$el.html(bookTemplate(this.model.attributes));
-        return this;
-    }
-});
-
 //создаем коллекцию экземпляров модели
 const Library = Backbone.Collection.extend({
   model: LinkModel
 });
 
-const OneLibrary = Backbone.Collection.extend({
-    model: OneModel
-});
 
 
 let Router = Backbone.Router.extend({
@@ -75,20 +39,31 @@ let Router = Backbone.Router.extend({
         "post/:id":    "post"
     },
     index: function () {
-       console.log("index")
     },
     post: function () {
-        // alert(123);
     }
 });
+
+let library = new Library();    //создаем экземпляр коллекции
 
 let router = new Router(); // создаем экземпляр роутера
 // Backbone.history.start();  // Запускаем HTML5 History push
 Backbone.history.start({pushState: true});
 
+const RootView = Backbone.View.extend({
+    template: '<ul class="list"></ul>',
+    initialize () {
+        library.on('add', (model) => {(new LinkView({model:model})).render().$el.appendTo('.list')});
+    },
+    render: function() {
+        this.$el.html(this.template);
+        return this;
+    }
+});
 
-let library = new Library();    //создаем экземпляр коллекции
-library.on('add', (model) => {(new LinkView({model:model})).render().$el.appendTo('.list')});
+let rootView = new RootView();
+
+$('.app').html(rootView.render().$el)
 library.fetch({ url: 'https://jsonplaceholder.typicode.com/posts' });
 
 
