8282fc142cb653dec9a226bf368b68045b4ccf23
barck <artiv09@gmail.com>
2018-02-12 17:26:02 +0500

    next stage

--- app/views/pages/usersView.js
+++ app/views/pages/usersView.js
@@ -1,8 +1,38 @@
 import Backbone from "backbone";
 import usersTemplate from "../../templates/pages/users.html";
+import userItemTemplate from "../../templates/items/userItem.html";
+import $ from "jquery";
+
+const UserModel = Backbone.Model.extend({
+    defaults:{
+    }
+});
+
+const UserCollection = Backbone.Collection.extend({
+    model: UserModel
+});
+
+const UserView = Backbone.View.extend({
+    tagName: "li",
+    className: "user",
+    render: function () {
+        this.$el.html(userItemTemplate(this.model.attributes));
+        return this;
+    }
+});
 
 const UsersView = Backbone.View.extend({
-
+    initialize(){
+        $(".app").addClass("loading");
+        let userCollection = new UserCollection();
+        userCollection.fetch({"url": "http://jsonplaceholder.typicode.com/users"});
+        userCollection.on("add", (model) => {
+            $(".app").removeClass("loading");
+            let userView = new UserView({model:model});
+            // userView.render().$el.appendTo(".users");
+            this.$el.find('.users').append(userView.render().el);
+        })
+    },
     render: function () {
         this.$el.html(usersTemplate());
         return this;
