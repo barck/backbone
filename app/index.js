bbb9858e1bb2b1f9800fff462cfe30f49251f537
barck <artiv09@gmail.com>
2018-02-17 13:52:58 +0500

    active menu in proggress

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import _ from 'underscore';
 import Backbone from 'backbone';
 import navLinkView from "./views/NavLinkView.js";
+import navLinkModel from "./models/NavLinkModel.js"
 
 Backbone.history.start(/*{pushState: true}*/);
 
